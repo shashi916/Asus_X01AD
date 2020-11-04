50701484dda4845b7cba218523dec58adde3924c
shashi916 <sashi5967@gmail.com>
2020-11-04 14:40:39 +0530

    Revert "PM / s2idle: Rename ->enter_freeze to ->enter_s2idle"
    This reverts commit fb7f9ca8f5494484b5d373ffe0d736b31b8ea0bc.

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -166,7 +166,7 @@ static void cpuidle_idle_call(void)
 	 * until a proper wakeup interrupt happens.
 	 */
 	if (idle_should_freeze()) {
-		entered_state = cpuidle_enter_s2idle(drv, dev);
+		entered_state = cpuidle_enter_freeze(drv, dev);
 		if (entered_state > 0) {
 			local_irq_enable();
 			goto exit_idle;
